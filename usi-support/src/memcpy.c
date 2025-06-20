adc7349074564a8ab6ad8f11efe9ac21ffdeee9f
Connor Horman <chorman64@gmail.com>
2025-06-20 13:57:57 -0400

    Fix memcmp impl

--- usi-support/src/memcpy.c
+++ usi-support/src/memcpy.c
@@ -19,7 +19,7 @@ void* memmove(void* __dest, const void* __src, unsigned long len) {
 }
 
 int memcmp(const void* __a, const void* __b, unsigned long len){
-    const char* __a_end = (const char*)__a;
+    const char* __a_end = ((const char*)__a)+len;
     for(const char* __a_ptr = (const char*)__a, *__b_ptr = (const char*)__b; __a_ptr!=__a_end; __a++, __b++){
         if((*__a_ptr)<(*__b_ptr))
             return -1;
